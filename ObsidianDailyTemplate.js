58b036afba5365a7c12d97905b52ce42dfc5ff2c
Doug Safford <doug.safford@yahoo.com>
2022-10-06 20:56:54 -0500

    semi annual start of daily  template added

---
+++ ObsidianDailyTemplate.js
@@ -0,0 +1,50 @@
+// This creates a weekly obsidian file on my macs, filepath based on machine
+// node obsidianweeklytemplate.js 41  is the command line call.  Need the desired month added to the call
+// note:  call can be used from keyboard maestro macro and called that way...
+
+const moment = require('moment')
+const fs = require('fs');
+
+const { exit } = require('process');
+
+const inputWeek = process.argv[2];
+
+console.log('Week number input for template = ' + inputWeek);
+
+console.log('first day of week for input week = ' + moment().week(inputWeek).startOf().add(1, 'day').format('YYYY-MM-DD'));
+// exit()
+
+
+
+// const first = moment(dv.current().file.name);
+let filePath = '/Users/c023490/Vaults/DougVault/Calendar/2022/'
+const fileName = moment().format('YYYY') + '-W' + inputWeek + 'x.md';
+let result = "";
+
+let startTemplate = '# Weekly Review \n\n ### Theme for Week \n- [ ] \n\n\n ### Goals for Week \n- [ ] \n\n\n ### Overall \n\n ##### Home \n - [ ] \n\n\n ##### Work \n - [ ] '
+let firstDayDate = moment().week(inputWeek).startOf().add(1, 'day').format('YYYY-MM-DD')
+let seventhDayDate = moment().week(inputWeek).startOf().add(7, 'day').format('YYYY-MM-DD')
+
+let firstDay = "##### First day Number: ![[" + moment().week(inputWeek).startOf().add(1, 'day').format('YYYY-MM-DD') + "]]"
+let secondDay = "##### Second day Number: ![[" + moment().week(inputWeek).startOf().add(2, 'day').format('YYYY-MM-DD') + "]]"
+let thirdDay = "##### Third day Number: ![[" + moment().week(inputWeek).startOf().add(3, 'day').format('YYYY-MM-DD') + "]]"
+let forthDay = "##### Forth day Number: ![[" + moment().week(inputWeek).startOf().add(4
+    , 'day').format('YYYY-MM-DD') + "]]"
+let fifthDay = "##### Fifth day Number: ![[" + moment().week(inputWeek).startOf().add(5, 'day').format('YYYY-MM-DD') + "]]"
+let sixthDay = "##### Sixth day Number: ![[" + moment().week(inputWeek).startOf().add(6, 'day').format('YYYY-MM-DD') + "]]"
+let seventhDay = "##### SeventhDay Number: ![[" + moment().week(inputWeek).startOf().add(7, 'day').format('YYYY-MM-DD') + "]]"
+
+let gayleSection = "\n``` tracker\nsearchType: text\nsearchTarget: 'gayle:\\s+(?<value>[\\-]?[0-9]+[\\.][0-9]+|[\\-]?[0-9]+)'\nfolder: DougVault/Calendar/2022\nstartDate: " + firstDayDate + "\nendDate: " + seventhDayDate + "\nsummary:\n\ttemplate: 'Gayle interactions: {{ sum()}}'\n\tstyle: 'color: red; '\n```"
+
+let reframeSection = "\n``` tracker\nsearchType: text\nsearchTarget: 'gayle:\\s+(?<value>[\\-]?[0-9]+[\\.][0-9]+|[\\-]?[0-9]+)'\nfolder: DougVault/Calendar/2022\nstartDate: " + firstDayDate + "\nendDate: " + seventhDayDate + "\nsummary:\n\ttemplate: 'Reframe: {{ sum()}}'\n\tstyle: 'color: red; '\n```"
+
+let nlpSection = "\n``` tracker\nsearchType: text\nsearchTarget: 'gayle:\\s+(?<value>[\\-]?[0-9]+[\\.][0-9]+|[\\-]?[0-9]+)'\nfolder: DougVault/Calendar/2022\nstartDate: " + firstDayDate + "\nendDate: " + seventhDayDate + "\nsummary:\n\ttemplate: 'NLP: {{ sum()}}'\n\tstyle: 'color: red; '\n```"
+
+let meditationSection = "\n``` tracker\nsearchType: text\nsearchTarget: 'gayle:\\s+(?<value>[\\-]?[0-9]+[\\.][0-9]+|[\\-]?[0-9]+)'\nfolder: DougVault/Calendar/2022\nstartDate: " + firstDayDate + "\nendDate: " + seventhDayDate + "\nsummary:\n\ttemplate: 'Meditation: {{ sum()}}'\n\tstyle: 'color: red; '\n```"
+
+console.log(gayleSection);
+// exit()
+
+
+// Write out to markdown file in Vault
+fs.writeFileSync(filePath + fileName, startTemplate + '\n -------- \n' + firstDay + '\n' + secondDay + '\n' + thirdDay + '\n' + forthDay + '\n' + fifthDay + '\n' + sixthDay + '\n' + seventhDay + "\n\n\n" + gayleSection + "\n" + reframeSection + "\n" + nlpSection + "\n" + meditationSection)
---
+++ ObsidianSemiAnnualTemplate.js
@@ -0,0 +1,50 @@
+// This creates a quarterly obsidian file on my macs, filepath based on machine
+// node obsidianquarterlytemplate.js 3  is the command line call.  Need the desired quarter added to the call
+// note:  call can be used from keyboard maestro macro and called that way...
+
+
+const moment = require('moment')
+const fs = require('fs');
+const { exit } = require('process');
+
+const inputHalf = process.argv[2];
+
+// const inputQuarter = 4;
+
+if (inputHalf == 1) {
+    console.log('Year half input for template = ' + inputHalf);
+
+    console.log('Moment year half = ' + moment().month(1).format('MM-DD-YYYY'));
+    console.log('Moment Month 1 = ' + moment().month(0).format('MM'));
+    console.log('Moment Month 2 = ' + moment().month(1).format('MM'));
+    console.log('Moment Month 3 = ' + moment().month(2).format('MM'));
+    console.log('Moment Month 4 = ' + moment().month(3).format('MM'));
+    console.log('Moment Month 5 = ' + moment().month(4).format('MM'));
+    console.log('Moment Month 5 = ' + moment().month(5).format('MM'));
+} else {
+    console.log('Year half input for template = ' + 23);
+
+    console.log('Moment year half = ' + moment().month(1).format('MM-DD-YYYY'));
+    console.log('Moment Month 1 = ' + moment().month(6).format('MM'));
+    console.log('Moment Month 2 = ' + moment().month(7).format('MM'));
+    console.log('Moment Month 3 = ' + moment().month(8).format('MM'));
+    console.log('Moment Month 4 = ' + moment().month(9).format('MM'));
+    console.log('Moment Month 5 = ' + moment().month(10).format('MM'));
+    console.log('Moment Month 5 = ' + moment().month(11).format('MM'));
+}
+exit()
+
+
+let filePath = '/Users/c023490/Vaults/DougVault/Calendar/2022/'
+const fileName = moment().format('YYYY') + '-Q' + inputQuarter + '.md';
+let result = "";
+
+let startTemplate = '# Quarterly Review \n\n ### Theme for Quarter \n- [ ] \n\n\n ### Goals for Quarter \n- [ ] \n\n\n ### Overall \n\n ##### Home \n - [ ] \n\n\n ##### Work \n - [ ] '
+
+
+let firstMonth = "##### First Month Number: [[2022-" + moment().quarter(inputQuarter).startOf('quarter').format('MM') + "]]"
+let secondMonth = "##### Second Month Number: [[2022-" + moment().quarter(inputQuarter).startOf('quarter').add(1, 'month').format('MM') + "]]"
+let thirdMonth = "##### Third Month Number: [[2022-" + moment().quarter(inputQuarter).startOf('quarter').add(1, 'month').format('MM') + "]]"
+
+// Write out to markdown file in Vault
+fs.writeFileSync(filePath + fileName, startTemplate + '\n -------- \n' + firstMonth + '\n' + secondMonth + '\n' + thirdMonth)
